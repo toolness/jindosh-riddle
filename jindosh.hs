f051ba81e5d7b73365fa4902a3caa67d8f893233
Atul Varma <varmaa@gmail.com>
2016-12-17 22:23:29 -0500

    Add to jindosh

--- jindosh.hs
+++ jindosh.hs
@@ -1,5 +1,5 @@
-data Name = Winslow | Marcolla | Contee | Natsiou | Finsh
-  deriving (Show, Eq)
+data Name = Winslow | Marcolla | Contee | Natsiou | Finch
+  deriving (Show, Eq, Enum)
 
 data Color = Red | Green | Purple | Blue | White
   deriving (Show, Eq)
@@ -13,17 +13,50 @@ data Heirloom = Diamond | Tin | Pendant | Ring | Medal
 data Origin = Dunwall | Dabokva | Fraeport | Karnaca | Baleton
   deriving (Show, Eq)
 
-type Position = Integer
+data Position = FarLeft | SecondFromLeft | Center | SecondFromRight | FarRight
+  deriving (Show, Eq)
 
 data Person = Person {
-  name :: Name
-, color :: Color
-, drink :: Drink
-, heirloom :: Heirloom
-, origin :: Origin
-, position :: Position
+  name :: Maybe Name
+, color :: Maybe Color
+, drink :: Maybe Drink
+, heirloom :: Maybe Heirloom
+, origin :: Maybe Origin
+, position :: Maybe Position
 } deriving (Show, Eq)
 
-p = Person { name=Winslow, color=Red, drink=Whiskey, heirloom=Diamond, origin=Dunwall, position=1 }
+person = Person {
+  name = Nothing
+, color = Nothing
+, drink = Nothing
+, heirloom = Nothing
+, origin = Nothing
+, position = Nothing
+}
 
-boop = \a -> if drink a == Wine then color a == Purple else True
+simpleRules =
+  [ person { name=Just Contee, color=Just Red }
+  , person { position=Just FarLeft, name=Just Natsiou }
+  , person { position=Just SecondFromLeft, color=Just Green }
+  , person { position=Just Center, drink=Just Beer }
+  , person { drink=Just Wine, color=Just Purple }
+  , person { origin=Just Dabokva, color=Just White }
+  , person { name=Just Winslow, heirloom=Just Diamond }
+  , person { origin=Just Baleton, heirloom=Just Ring }
+  , person { name=Just Finch, drink=Just Absinthe }
+  , person { origin=Just Dunwall, drink=Just Whiskey }
+  , person { name=Just Marcolla, origin=Just Fraeport } ]
+
+-- "the lady in purple sat left of someone in blue"
+-- (so the lady in purple is NOT on the far right seat)
+
+-- "when one of the dinner guests bragged about her Snuff Tin,
+--  the woman next to her said they were finer in Dabokva, where she lived"
+-- (does this mean the snuff tin person has only one neighbor?)
+
+-- "someone else carried a valuable war medal, and when she saw it the
+--  visitor from karnaca next to her almost spilled her neighbor's rum"
+-- (does this mean the karnacan has two neighbors?)
+
+-- the lady from Dunwall fell onto the guest in the center seat
+-- (so the center seat is NOT from dunwall)
