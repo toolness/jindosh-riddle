db82e92796b414e19dfcc2c5636042c0a5f9217f
Atul Varma <varmaa@gmail.com>
2016-12-21 12:56:32 -0500

    my brain is slowly imploding

--- jindosh.hs
+++ jindosh.hs
@@ -34,8 +34,8 @@ nullPerson = Person { name=Nothing
 allNames :: [Name]
 allNames = (enumFrom (toEnum 0 :: Name))
 
-data Property x = Property { get :: Person -> Maybe x
-                           , set :: Person -> x -> Person }
+data Property x =Property { get :: Person -> Maybe x
+                          , set :: Person -> x -> Person }
 
 nameProp = Property { get=name, set=(\p x -> p { name=Just x }) }
 colorProp = Property { get=color, set=(\p x -> p { color=Just x }) }
@@ -44,15 +44,46 @@ heirloomProp = Property { get=heirloom, set=(\p x -> p { heirloom=Just x }) }
 originProp = Property { get=origin, set=(\p x -> p { origin=Just x }) }
 positionProp = Property { get=position, set=(\p x -> p { position=Just x }) }
 
-people :: [Person]
-people = map ((set nameProp) nullPerson) allNames
-
 type Constraint = [Person] -> Maybe [Person]
 
-simpleConstraint :: Property x -> x -> Property y -> y -> Constraint
+simpleConstraint :: (Eq x, Eq y) => Property x -> x -> Property y -> y -> Constraint
 simpleConstraint aprop a bprop b =
-  -- TODO: Actually implement this.
-  \x -> Nothing
+  let
+    isConstraintViolated person =
+      let
+        oneWay aprop a bprop b =
+          if ((get aprop) person) == Just a &&
+             ((get bprop) person) /= Nothing &&
+             ((get bprop) person) /= Just b then True else False
+      in
+        if oneWay aprop a bprop b || oneWay bprop b aprop a
+          then True else False
+    applyConstraint person =
+      let
+        oneWay aprop a bprop b person =
+          if ((get aprop) person) == Just a
+            then ((set bprop) person b)
+            else person
+        in
+          oneWay aprop a bprop b (oneWay bprop b aprop a person)
+    constraint people =
+      if any isConstraintViolated people
+        then Nothing
+        else Just (map applyConstraint people)
+  in
+    constraint
+
+applyConstraints constraints people =
+  let
+    applyConstraint constraint people =
+      case people of
+        Nothing -> Nothing
+        Just x -> constraint x
+  in
+    foldr applyConstraint (Just people) constraints
 
 constraints :: [Constraint]
 constraints = [ simpleConstraint nameProp Contee colorProp Red ]
+
+people :: [Person]
+people = map ((set nameProp) nullPerson) allNames
