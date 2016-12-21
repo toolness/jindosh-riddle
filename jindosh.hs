a76bf84519e3f258cc294e77c26017ac903151d7
Atul Varma <varmaa@gmail.com>
2016-12-21 10:25:50 -0500

    blerg jindosh haskell

--- jindosh.hs
+++ jindosh.hs
@@ -16,49 +16,21 @@ data Origin = Dunwall | Dabokva | Fraeport | Karnaca | Baleton
 data Position = FarLeft | SecondFromLeft | Center | SecondFromRight | FarRight
   deriving (Show, Eq)
 
-data Property = Name Name | Color Color | Drink Drink | Heirloom Heirloom |
-  Origin Origin | Position Position
+data Person = Person { name :: Maybe Name
+                     , color :: Maybe Color
+                     , drink :: Maybe Drink
+                     , heirloom :: Maybe Heirloom
+                     , origin :: Maybe Origin
+                     , position :: Maybe Position }
   deriving (Show, Eq)
 
-rule :: Property -> Property -> [(Property, Property)]
+allNames = (enumFrom (toEnum 0 :: Name))
 
-rule a b =
-  [(a, b), (b, a)]
-
-rulebook rules = rules
-
-rules = rulebook (
-  rule (Name Contee) (Color Red) ++
-  rule (Position FarLeft) (Name Natsiou) ++
-  rule (Position SecondFromLeft) (Color Green) ++
-  rule (Position Center) (Drink Beer) ++
-  rule (Drink Wine) (Color Purple) ++
-  rule (Origin Dabokva) (Color White) ++
-  rule (Name Winslow) (Heirloom Diamond) ++
-  rule (Origin Baleton) (Heirloom Ring) ++
-  rule (Name Finch) (Drink Absinthe) ++
-  rule (Origin Dunwall) (Drink Whiskey) ++
-  rule (Name Marcolla) (Origin Fraeport)
-  )
-
--- "the lady in purple sat left of someone in blue"
--- (so the lady in purple is NOT on the far right seat.
-    also not in second from left, or far left. second from right is wearing white, so she can't be in center. )
-
--- "when one of the dinner guests bragged about her Snuff Tin,
---  the woman next to her said they were finer in Dabokva, where she lived"
--- (does this mean the snuff tin person has only one neighbor?)
---   (if so, it's not the far left, b/c then the second-from-left would
---    be wearing both green AND white which is impossible, so it's far right.
---    this means second from right is from dabovka and wearing white.)
-
--- "someone else carried a valuable war medal, and when she saw it the
---  visitor from karnaca next to her almost spilled her neighbor's rum"
--- (does this mean the karnacan has two neighbors?)
---   (if so, the karnacan is either at center or second from left, since
---    the lady from dabovka is second from right.)
-
---    if the karnacan is second from left: she is wearing green
-
--- the lady from Dunwall fell onto the guest in the center seat
--- (so the center seat is NOT from dunwall)
+people = map
+  (\name -> Person { name=Just name
+                   , color=Nothing
+                   , drink=Nothing
+                   , heirloom=Nothing
+                   , origin=Nothing
+                   , position=Nothing })
+  allNames
