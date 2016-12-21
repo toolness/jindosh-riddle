9ee6940d0c0351eba6a866b61e7ea22932e08dbe
Atul Varma <varmaa@gmail.com>
2016-12-21 11:29:42 -0500

    More work on jindosh.hs.

--- jindosh.hs
+++ jindosh.hs
@@ -24,13 +24,35 @@ data Person = Person { name :: Maybe Name
                      , position :: Maybe Position }
   deriving (Show, Eq)
 
+nullPerson = Person { name=Nothing
+                    , color=Nothing
+                    , drink=Nothing
+                    , heirloom=Nothing
+                    , origin=Nothing
+                    , position=Nothing }
+
+allNames :: [Name]
 allNames = (enumFrom (toEnum 0 :: Name))
 
-people = map
-  (\name -> Person { name=Just name
-                   , color=Nothing
-                   , drink=Nothing
-                   , heirloom=Nothing
-                   , origin=Nothing
-                   , position=Nothing })
-  allNames
+data Property x = Property { get :: Person -> Maybe x
+                           , set :: Person -> x -> Person }
+
+nameProp = Property { get=name, set=(\p x -> p { name=Just x }) }
+colorProp = Property { get=color, set=(\p x -> p { color=Just x }) }
+drinkProp = Property { get=drink, set=(\p x -> p { drink=Just x }) }
+heirloomProp = Property { get=heirloom, set=(\p x -> p { heirloom=Just x }) }
+originProp = Property { get=origin, set=(\p x -> p { origin=Just x }) }
+positionProp = Property { get=position, set=(\p x -> p { position=Just x }) }
+
+people :: [Person]
+people = map ((set nameProp) nullPerson) allNames
+
+type Constraint = [Person] -> Maybe [Person]
+
+simpleConstraint :: Property x -> x -> Property y -> y -> Constraint
+simpleConstraint aprop a bprop b =
+  -- TODO: Actually implement this.
+  \x -> Nothing
+
+constraints :: [Constraint]
+constraints = [ simpleConstraint nameProp Contee colorProp Red ]
